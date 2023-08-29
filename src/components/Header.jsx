b3d1bbc19fa81fc3be424beb134c35f3a23fcd21
akorede <emiolasaheed1@gmail.com>
2023-08-29 10:55:12 +0100

    Create website header with empty nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import planet from '../assets/planet.png'
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
