0b9b63f8eaa6e1a63bd54fe31368193284535451
akorede <emiolasaheed1@gmail.com>
2023-08-29 13:38:22 +0100

    Set up Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+import Layout from './components/Layout';
+import RocketsView from './features/rockets/RocketsView';
+import MissionsView from './features/missions/MissionsView';
+import ProfileView from './features/profile/ProfileView';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<Layout />}>
+    <Route index path="/" element={<RocketsView />} />
+    <Route path="missions" element={<MissionsView />} />
+    <Route path="profile" element={<ProfileView />} />
+  </Route>,
+));
+
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
