0bbaaa1beba609b4303e0120aca7d876f7f7eca0
akorede <emiolasaheed1@gmail.com>
2023-08-29 11:16:42 +0100

    Set up linters

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import planet from '../assets/planet.png'
-const Header = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li></li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
+import React from 'react';
+// import planet from '../assets/planet.png';
 
-export default Header
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li />
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
