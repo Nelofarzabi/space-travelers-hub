681d1708e1323db1b5666c62297e9ddf534f41c9
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-08-30 18:07:29 +0430

    Display mission in profile

--- src/features/profile/ProfileView.jsx
+++ src/features/profile/ProfileView.jsx
@@ -4,15 +4,26 @@ import './Profile.css';
 const ProfileView = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const missions = useSelector((state) => state.missions.missions);
+  const reservedMissions = missions.filter((mission) => mission.joined);
   return (
     <main className="profile--container">
       <section>
         <h2>My Missions</h2>
-        <ul className="profile--list">
-          <li>Teslar</li>
-          <li>Teslar</li>
-          <li>Teslar</li>
-        </ul>
+        {
+          reservedMissions.length > 0
+            ? (
+              <ul className="profile--list">
+                {
+            reservedMissions.map((mission) => {
+              const { name, id } = mission;
+              return <li key={id}>{name}</li>;
+            })
+          }
+              </ul>
+            )
+            : <p className="list--info--para">No missions joined at the moment</p>
+        }
       </section>
       <section>
         <h2>My Rockets</h2>
