bb3f1b02c3c39c4b56607d35c03a6127b010562c
akorede <emiolasaheed1@gmail.com>
2023-08-30 10:02:31 +0100

    Render the fetched missions

--- src/features/missions/MissionsView.jsx
+++ src/features/missions/MissionsView.jsx
@@ -7,9 +7,8 @@ const MissionsView = () => {
   const dispatch = useDispatch();
   const { missions } = useSelector((state) => state.missions);
   useEffect(() => {
-    console.log(missions);
     dispatch(fetchMissions());
-  }, []);
+  }, [dispatch]);
   return (
     <main>
       <table>
@@ -24,27 +23,25 @@ const MissionsView = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>Thaicom</th>
-            <td>
-              Thaicom is the name of a series of communications
-              satellites operated from Thailand, and also the name
-              of Thaicom Public Company Limited, which is the company
-              that owns and operates the Thaicom satellite fleet and
-              other telecommunication businesses in Thailand and
-              throughout the Asia-Pacific region. The satellite projects
-              were named Thaicom by the King of Thailand, His Majesty
-              the King Bhumibol Adulyadej, as a symbol of the linkage
-              between
-              Thailand and modern communications technology.
-            </td>
-            <td>
-              <button type="button" className="member--btn">not a member</button>
-            </td>
-            <td>
-              <button type="button" className="join--mission--btn">Join Mission</button>
-            </td>
-          </tr>
+          {
+            missions.map((mission) => {
+              const { name, id, description } = mission;
+              return (
+                <tr key={id}>
+                  <th>{name}</th>
+                  <td>
+                    {description}
+                  </td>
+                  <td>
+                    <button type="button" className="member--btn">not a member</button>
+                  </td>
+                  <td>
+                    <button type="button" className="join--mission--btn">Join Mission</button>
+                  </td>
+                </tr>
+              );
+            })
+          }
         </tbody>
       </table>
     </main>
