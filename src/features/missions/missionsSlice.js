6036af5705c74db12f6fa2a720090bc245e1af05
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-01 11:17:26 +0430

    Add test for mission

--- src/features/missions/missionsSlice.js
+++ src/features/missions/missionsSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 const initialState = {
   missions: [],
@@ -9,7 +8,8 @@ const initialState = {
 
 export const fetchMissions = createAsyncThunk('missions/fetchMissions', async (_, { rejectWithValue }) => {
   try {
-    const { data } = await axios.get('https://api.spacexdata.com/v3/missions');
+    const response = await fetch('https://api.spacexdata.com/v3/missions');
+    const data = await response.json();
     return data;
   } catch (error) {
     return rejectWithValue(error.message);
